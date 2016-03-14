32bd8991a03f610959272750c9ba40c2cf5a2e77
hoale <ledanghoa1984@outlook.com>
2016-03-14 21:08:02 +0700

    add todos/:id

--- server.js
+++ server.js
@@ -4,12 +4,48 @@
 
 var express = require('express');
 var app = express();
+var _ = require('lodash');
 var PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-   res.send('TO DO API');
+var todos = [
+    {
+        id: 1,
+        description: 'Meeting with client',
+        complete: true
+    },
+    {
+        id: 2,
+        description: 'Go to bank',
+        complete: true
+    },
+    {
+        id: 3,
+        description: 'Go to the zoo',
+        complete: false
+    }
+];
+
+app.get('/todos', function (req, res) {
+    res.json(todos);
+});
+app.get('/todos/:id', function (req, res) {
+    var todoId = parseInt(req.params.id, 10);
+    console.log(todoId);
+    var matchTodo = _.find(todos,{id: todoId});
+
+    if (matchTodo) {
+        res.json(matchTodo);
+    }
+    else {
+        res.status(404).send();
+    }
+
 });
 
-app.listen(PORT, function(){
-    console.log('Express is running on '+ PORT);
+app.get('/', function (req, res) {
+    res.send('TO DO API');
+});
+
+app.listen(PORT, function () {
+    console.log('Express is running on ' + PORT);
 });
